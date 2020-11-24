84549dadf3c60428ae43c998885e171b9a3455c2
Luc Patiny <luc@patiny.com>
2020-11-24 16:43:31 +0100

    chore: add calculate MF example

--- src/__tests__/appendMolfile.test.js
+++ src/__tests__/appendMolfile.test.js
@@ -1,5 +1,8 @@
+import delay from 'delay';
 import { readFileSync } from 'fs';
-import {join} from 'path';
+import { join } from 'path';
+import { MF } from 'mf-parser';
+import OCL from 'openchemlib';
 
 import appendMolfile from '../parser/appendMolfile.js';
 
@@ -7,12 +10,19 @@ describe('appendMolfile', () => {
   it('should fetch the molfile from URL and return molfile in plain text', async () => {
     const path = join(__dirname, '../../data/JSON-results.json');
     const singleProducts = JSON.parse(readFileSync(path));
-    let molfiles = []
-    console.log(singleProducts[150].iupac);
+    let molfiles = [];
     let test = await appendMolfile(singleProducts[150]);
-    // for (let result of singleProducts){
-    //     molfiles.push(await appendMolfile(result));
-    // }
+    for (let result of singleProducts.slice(10, 13)) {
+      let molfile = await appendMolfile(result);
+      let molecule = OCL.Molecule.fromMolfile(molfile);
+      let mf = molecule.getMolecularFormula().formula;
+      let mfInfo = new MF(mf).getInfo();
+
+      console.log(mfInfo);
+      molfiles.push(molfile);
+      await delay(1000);
+      console.log('done');
+    }
     console.log(test);
     expect(singleProducts.length).toBeGreaterThan(0);
   });
--- src/parser/appendMolfile.js
+++ src/parser/appendMolfile.js
@@ -1,25 +1,12 @@
 import fetch from 'node-fetch';
-import { URLToolkit } from 'url-toolkit';
-import urlencode from 'urlencode';
 
 export default async function appendMolfile(result) {
   const name = result.iupac;
-  let url = encodeURI(`http://46.4.119.202:8082/?name=${name}&what=molfile`);
-//   console.log(url);
-  let molfile = await fetch(url, {
-    headers: {
-      accept:
-        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6',
-      'cache-control': 'max-age=0',
-      'if-modified-since': 'Tue, 24 Nov 2020 12:28:50 GMT',
-      'upgrade-insecure-requests': '1',
-    },
-    referrerPolicy: 'strict-origin-when-cross-origin',
-    body: null,
-    method: 'GET',
-    mode: 'cors',
-    credentials: 'omit',
-  }).then((res) => res.text());
+  let url = `http://46.4.119.202:8082/?name=${encodeURIComponent(
+    name,
+  )}&what=molfile`;
+  let response = await fetch(url);
+  let molfile = await response.text();
+  console.log(molfile);
   return molfile;
 }
